9f1391df757324318d82beee54a610fb8508faac
Aziz Smaoui <ahmed.aziz.smaoui@gmail.com>
2023-06-13 21:31:04 +0100

    employee table config

--- employee_management/website/models.py
+++ employee_management/website/models.py
@@ -1,7 +1,20 @@
 from django.db import models
-
+from django_enumfield import enum
 # Create your models here.
 
 
+class status(enum.Enum):
+    available = 0
+    not_available = 1
+    days_off = 2
+    
+
 class employee(models.Model):
-    name = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
+    date_birth = models.DateField(null=True)
+    position = models.CharField(max_length=255,default="")
+    status = enum.EnumField(status , default=status.available)
+    date_return = models.DateField(null=True)
+    email = models.EmailField(default="")
+    phone = models.CharField(default="")
+    
